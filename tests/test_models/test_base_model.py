fa20a9862fecf1fe9a4b3dfaf5f3ac373b49a6fb
ubonisrael <akpanudohubonisrael@gmail.com>
2024-01-13 15:41:02 +0100

    added AUTHORs file

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines tests for the BaseModel of the project"""
+import unittest
+
+
+class TestBaseModel_Init(unittest.TestCase):
+    """Tests for the initialization of the BaseModel class"""
+    
